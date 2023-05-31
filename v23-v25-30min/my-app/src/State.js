50c69f640b2c1526bbc8d4d199b3baae4e43d3e4
lina-22 <haquefahinur@gmail.com>
2023-06-01 01:24:55 +0200

    working on state handler

---
+++ v23-v25-30min/my-app/src/State.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class State extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>Count : {count}</h1>
+        <button
+          onClick={this.handleIncrement}
+          disabled={count === 5 ? true : false}>
+          +
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          disabled={count === 0 ? true : false}>
+          -
+        </button>
+        {/* <button onClick={this.handleDecrement} disabled={count===0 && true}>-</button> */}
+      </div>
+    );
+  }
+}

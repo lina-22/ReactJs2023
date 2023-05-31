c2d7e4a20f30990ada96eef60f35fbd355b16d2d
lina-22 <haquefahinur@gmail.com>
2023-05-31 23:57:01 +0200

    working on state handling

---
+++ v21-v22-30min/my-app/src/Component/ReactBootstrap.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+const ReactBootstrap = () => {
+  return (
+    <div>
+      <h1>Card Title</h1>
+      <p>Bismillahir rahmanir rahim Bismillahir rahmanir rahim </p>
+      <Button>Learn More</Button>
+    </div>
+  );
+};
+
+export default ReactBootstrap;

6cd087a79bf70793efff4ddef0208836fa02864d
lina-22 <haquefahinur@gmail.com>
2023-06-13 21:54:46 +0200

    react handleincrement and handle decrement function done

--- v23-v25-30min/my-app/src/App.js
+++ v23-v25-30min/my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 // import State from "./State";
 import SecondSt from "./SecondState";
-
+import ThirdSt from "./ThirdState";
 export default function App() {
   return (
     <div>
       {/* <State /> */}
       {/* <SecondSt count="0" /> */}
-      <SecondSt />
+      {/* <SecondSt /> */}
+      <ThirdSt />
     </div>
   );
 }

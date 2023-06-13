40b04d780a3af0c4c25bd84b52d5d860ccf61fbb
lina-22 <haquefahinur@gmail.com>
2023-06-13 21:24:23 +0200

    react handleincrement and handle decrement function done

--- v23-v25-30min/my-app/src/App.js
+++ v23-v25-30min/my-app/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import State from "./State";
+// import State from "./State";
+import SecondSt from "./SecondState";
 
 export default function App() {
   return (
     <div>
-      <State />
+      {/* <State /> */}
+      {/* <SecondSt count="0" /> */}
+      <SecondSt />
     </div>
   );
 }
---
+++ v23-v25-30min/my-app/src/SecondState.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class SecondState extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      msg: "",
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Count : {count}</h1>
+        <button
+          onClick={this.handleIncrement}
+          disabled={count === 10 ? true : false}>
+          +
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          disabled={count === 0 ? true : false}>
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SecondState;

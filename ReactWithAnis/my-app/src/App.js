bb45caaef844fffb5ad226df28b13c2853a6bdee
lina-22 <haquefahinur@gmail.com>
2023-08-02 20:46:55 +0200

    revise

--- ReactWithAnis/my-app/src/App.js
+++ ReactWithAnis/my-app/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Card from "./components/Card";
+import Card from "./componentsArrayPropsDestructuring/Card";
+import Data from "./data.json";
 
+// console.log(Data[0]);
 export default function App() {
   return (
     <div>
       <h1 className="headingStyle">Todo App</h1>
-      <Card />
-      <Card />
-      <Card />
+      {/* <Card
+        className=""
+        titleText="Call Mother"
+        descriptionText="This is description 1"
+      /> */}
+      <Card titleText={Data[0].title} descriptionText={Data[0].desc} />
+      <Card titleText={Data[1].title} descriptionText={Data[1].desc} />
     </div>
   );
 }
---
+++ ReactWithAnis/my-app/src/componentsArrayPropsDestructuring/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+const date = "26/07/2023";
+const headingStyle = {
+  backgroundColor: "purple",
+  color: "white",
+};
+
+const name = ["dog", "cat"];
+const [dog, cat] = name;
+export default function Card(props) {
+  // console.log(props);
+  const { titleText, descriptionText } = props;
+  return (
+    <div>
+      <h2 style={headingStyle}>{titleText}</h2>
+      <h2 className="headingStyle">{props.descriptionText}</h2>
+      <h4>{new Date().getFullYear()}</h4>
+    </div>
+  );
+}

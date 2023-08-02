8947ee808c74a76c630d2449752600a7b21544e6
lina-22 <haquefahinur@gmail.com>
2023-08-02 18:01:59 +0200

    components added

--- ReactWithAnis/my-app/src/App.js
+++ ReactWithAnis/my-app/src/App.js
@@ -4,6 +4,7 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div>
+      <h1 className="headingStyle">Todo App</h1>
       <Card />
       <Card />
       <Card />

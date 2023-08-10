d7fef8b098b6a21300cefd23f03b733d24decfc6
lina-22 <haquefahinur@gmail.com>
2023-08-10 11:07:27 +0200

    HooksUpdates

--- v23-30-90min/statemanage/src/HooksUseState/index1.js
+++ v23-30-90min/statemanage/src/HooksUseState/index1.js
@@ -6,10 +6,25 @@ export default function Index1() {
   const handleIncrement = () => {
     setCount(count + 1);
   };
+
+  const handleIncrement2 = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
+  const handleIncrement3 = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
+
   return (
     <div>
       <h1>Count :{count}</h1>
-      <button onClick={handleIncrement}>Increment Hooks</button>
+      <button onClick={handleIncrement}>Hoks+</button>
+      <button onClick={handleIncrement2}>HoksPrev+</button>
+      <button onClick={handleIncrement3}>HoksPrev+</button>
     </div>
   );
 }

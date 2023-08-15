556ffc6a8c29f08049fd7dd4561449d23c0e89cb
lina-22 <haquefahinur@gmail.com>
2023-08-15 16:22:41 +0200

    react toastify

--- v41-v43-30min/useeffectapp/src/App.js
+++ v41-v43-30min/useeffectapp/src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Datafetch from "./HOOKs/CustomHooks/Datafetch1";
 
 // import UseEffectExemple from "./HOOKs/UseEffect/UseEffectExemple";
 
 function App() {
+  const handleAddNewTodo = () => {
+    toast("new todo is added");
+  };
+  const handleDeleteNewTodo = () => {
+    toast("new todo is deleted");
+  };
   return (
     <div className="App">
-      <Datafetch />
+      {/* <Datafetch /> */}
+      <h2>Todo App</h2>
+      <button onClick={handleAddNewTodo}>Add New Todo</button>
+      <button onClick={handleDeleteNewTodo}>Delete New Todo</button>
+      <ToastContainer />
     </div>
   );
 }

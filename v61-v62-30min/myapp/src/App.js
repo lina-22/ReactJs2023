5b55063f316abbea4762a9d24abfe0891ba69f02
lina-22 <haquefahinur@gmail.com>
2023-08-29 20:07:38 +0200

    react routing | dynamic routing using useParams

--- v61-v62-30min/myapp/src/App.js
+++ v61-v62-30min/myapp/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Blogs from "./pages/Blogs";
+import Blog from "./pages/Blog";
 import Error from "./pages/Error";
 import Navbar from "./components/Navbar";
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:title" element={<Blog />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
--- v61-v62-30min/myapp/src/pages/Blogs.js
+++ v61-v62-30min/myapp/src/pages/Blogs.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { blogsData } from "../data";
+import { Link } from "react-router-dom";
 
 export default function Blogs() {
+  const [blogs, setBlogs] = useState(blogsData);
+
+  const truncateString = (str, num) => {
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
   return (
     <div>
       <h1>Blogs page</h1>
-      <p>
-        hello i am kaisan kaisan, hello i am kaisan kaisan, hello i am kaisan
-        kaisan, hello i am kaisan kaisan
-      </p>
-      <p>
-        hello i am kaisan kaisanhello i am kaisan kaisanhello i am kaisan kaisan
-        hello i am kaisan kaisan hello i am kaisan kaisan hello i am kaisan
-        kaisan hello i am kaisan kaisan hello i am kaisan kaisan hello i am
-        kaisan kaisan hello i am kaisan kaisan
-      </p>
+      <section>
+        {blogs.map((blog) => {
+          const { id, title, body } = blog;
+          return (
+            <article key={id}>
+              <h3>{title}</h3>
+              <p>{truncateString(body, 100)}</p>
+              <Link to={title}>Learn More</Link>
+            </article>
+          );
+        })}
+      </section>
     </div>
   );
 }
